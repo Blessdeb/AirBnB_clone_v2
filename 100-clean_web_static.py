7abc2c1fc4f1259e94a86b0e543ec46002d4a3d8
Blessdeb <oyemadedeborah@gmail.com>
2024-05-06 13:26:53 -0700

    static

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,89 +1,33 @@
 #!/usr/bin/python3
 """
-Fabric script that creates and distributes an archive to your web servers
+Deletes out-of-date archives
+fab -f 100-clean_web_static.py do_clean:number=2
+    -i ssh-key -u ubuntu > /dev/null 2>&1
 """
-from fabric.api import env, run, put, local
-from os.path import exists
 
-# Define your server IPs and user
-env.hosts = ['100.26.168.24', '100.26.216.185']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'  # Define your SSH private key here
+import os
+from fabric.api import *
 
-
-def do_pack():
-    """
-    Generates a .tgz archive from the contents of the web_static folder
-    """
-    try:
-        local("mkdir -p versions")
-        file_name = "versions/web_static_$(date '+%Y%m%d%H%M%S').tgz"
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except Exception as e:
-        return None
-
-
-def do_deploy(archive_path):
-    """
-    Deploys the archive to the web servers.
-    """
-    if not exists(archive_path):
-        print("Archive not found: {}".format(archive_path))
-        return False
-
-    # Extract file name and directory paths from archive_path
-    file_name = archive_path.split("/")[-1]
-    no_ext = file_name.replace('.tgz', '')
-    dest_path = "/data/web_static/releases/" + no_ext + "/"
-
-    try:
-        # Upload archive to the /tmp/ directory
-        put(archive_path, "/tmp/" + file_name)
-
-        # Create directory where we will uncompress
-        run("mkdir -p {}".format(dest_path))
-
-        # Uncompress the archive to the folder on the server
-        run("tar -xzf /tmp/{} -C {}".format(file_name, dest_path))
-
-        # Delete the archive from the server
-        run("rm /tmp/{}".format(file_name))
-
-        # Move contents out of the 'web_static' folder from the tar archive
-        run("mv {0}/web_static/* {0}/".format(dest_path))
-
-        # Remove the empty directory
-        run("rm -rf {}/web_static".format(dest_path))
-
-        # Delete the old symbolic link
-        run("rm -rf /data/web_static/current")
-
-        # Create a new symbolic link
-        run("ln -s {} /data/web_static/current".format(dest_path))
-
-        print("New version deployed!")
-        return True
-    except Exception as e:
-        print(f"Deployment failed: {e}")
-        return False
+env.hosts = ['100.26.216.185', '100.26.168.24']
 
 
 def do_clean(number=0):
+    """Delete out-of-date archives.
+    Args:
+        number (int): The number of archives to keep.
+    If number is 0 or 1, keeps only the most recent archive. If
+    number is 2, keeps the most and second-most recent archives,
+    etc.
     """
-    Deletes out-of-date archives.
-    """
-    try:
-        # Delete out-of-date archives in the versions folder
-        local("ls -1t versions | tail -n +{} \
-              | xargs -I {{}} rm versions/{{}}".format(number + 1))
+    number = 1 if int(number) == 0 else int(number)
 
-        # Delete out-of-date archives in the
-        # /data/web_static/releases folder on the web servers
-        run("ls -1t /data/web_static/releases | \
-            tail -n +{} | xargs -I {{}} rm -rf \
-            /data/web_static/releases/{{}}".format(number + 1))
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
 
-        print("Cleaned up archives.")
-    except Exception as e:
-        print(f"Cleaning failed: {e}")
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
