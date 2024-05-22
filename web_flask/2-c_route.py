e0355ac33a472f9114e7ab96fddf946c9dc68660
Blessdeb <oyemadedeborah@gmail.com>
2024-05-22 15:28:04 -0700

    thefhfhh

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,3 +17,12 @@ def index():
 def hbnb():
     """returns HBNB"""
     return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
